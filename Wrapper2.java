08d9d5cc04f0d9b2f1a314f87e09d00baed42114
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-23 18:08:27 +0530

    Add files via upload

---
+++ Wrapper2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Wrapper2
+{
+    int num;
+    void getVal()
+     {
+		  Scanner s = new Scanner(System.in);
+          System.out.println("Decimal to HexaDecimal,Octal and Binary");
+          
+          System.out.println("\nEnter the number :");
+          num = Integer.parseInt(s.nextLine());
+     }
+    void convert()
+      {
+           String hexa = Integer.toHexString(num);
+           System.out.println("HexaDecimal Value is : " + hexa);
+           
+		   String octal = Integer.toOctalString(num);
+           System.out.println("Octal Value is : " + octal);
+           
+		   String binary = Integer.toBinaryString(num);
+           System.out.println("Binary Value is : " + binary);
+       }
+
+   public static void main(String args[])
+    {
+        Two t1 = new Two();
+         t1.getVal();
+         t1.convert();
+    }
+}
